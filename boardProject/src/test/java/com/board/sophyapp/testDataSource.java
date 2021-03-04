39893f7ba81f08fd665d711a5d64a4324b2d96a3
Sophy <Sophy@DESKTOP-OV581HU>
2021-03-04 22:52:00 +0900

    Spring + MariaDB 연동 test 진행
    1) pom.xml 설정 ( mariadb를 위한 )
    2) roo-context.xml 설정
    3) mybatis, mybatis-config.xml, mapper 생성
    4) Junit test 완료
    5) 오류 발생
      - Caused by: org.xml.sax.SAXParseException; 예기치 않은 파일의 끝입니다.
      - mapper.xml 에 namespace 적용을 하지 않아 발생된 문제. (해결)

---
+++ boardProject/src/test/java/com/board/sophyapp/testDataSource.java
@@ -0,0 +1,37 @@
+package com.board.sophyapp;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class testDataSource {
+
+
+	 @Inject
+	 private DataSource dataSource;
+
+	@Test
+	public void test() throws Exception {
+		//fail("Not yet implemented");
+		try(Connection con = dataSource.getConnection()) {
+			System.out.println("DataSource 설정 성공!!!!!!!!!!");
+			System.out.println(con);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("실패");
+			e.printStackTrace();
+		}
+
+	}
+
+}
